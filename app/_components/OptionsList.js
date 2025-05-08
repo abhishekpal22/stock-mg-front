4166e79311fd3408f39be22d63bbe40deee00fb1
abhishek <abhishekpal2500@gmail.com>
2025-05-08 20:34:04 +0530

    optlist updated

--- app/_components/OptionsList.js
+++ app/_components/OptionsList.js
@@ -1,18 +1,15 @@
 import React, { useEffect } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import { useDeleteDataMutation, useGetDataQuery,  } from '../utils/api/apiSlice';
-import { delete_option, options_list,  } from '../utils/api/apiURLs';
+import { useDeleteDataMutation, useGetDataQuery, } from '../utils/api/apiSlice';
+import { delete_option, options_list, } from '../utils/api/apiURLs';
 import { toCamelCase } from '../utils/commonFunctions';
 import { Box, Card, Chip, Grid, Typography } from '@mui/material';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import IconButton from '@mui/material/IconButton';
 
-
-
-
 const paginationModel = { page: 0, pageSize: 5 };
 
-const  OptionsList = ({ refreshTrigger }) => {
+const OptionsList = ({ refreshTrigger }) => {
 
     const isValidColor = (color) => {
         const s = new Option().style;
@@ -33,17 +30,19 @@ const  OptionsList = ({ refreshTrigger }) => {
         { field: 'id', headerName: 'Sr.N.', width: 30 },
         { field: 'productName', headerName: 'Product Name', flex: 1, minWidth: 150 },
         {
-            field: 'action', headerName: 'Action', minWidth: 80, 
+            field: 'action', headerName: 'Action', minWidth: 80,
             sortable: false,
             renderCell: renderAction(handleDelete),
         },
     ];
-    const shopFromColumns =(handleDelete) => [
+    const shopFromColumns = (handleDelete) => [
         { field: 'id', headerName: 'Sr.N.', width: 30 },
         { field: 'shopFrom', headerName: 'Shop From', flex: 1, minWidth: 150 },
-        { field: 'action', headerName: 'Action', minWidth: 80 , 
+        {
+            field: 'action', headerName: 'Action', minWidth: 80,
             sortable: false,
-            renderCell: renderAction(handleDelete),},
+            renderCell: renderAction(handleDelete),
+        },
     ]
     const colorColumns = (handleDelete) => [
         { field: 'id', headerName: 'Sr.N.', width: 30 },
@@ -76,23 +75,29 @@ const  OptionsList = ({ refreshTrigger }) => {
                 );
             },
         },
-        { field: 'action', headerName: 'Action', minWidth: 80, 
+        {
+            field: 'action', headerName: 'Action', minWidth: 80,
             sortable: false,
-            renderCell: renderAction(handleDelete) },
+            renderCell: renderAction(handleDelete)
+        },
     ]
     const brandColumns = (handleDelete) => [
         { field: 'id', headerName: 'Sr.N.', width: 30 },
         { field: 'brand', headerName: 'Brand Name', flex: 1, minWidth: 100, },
-        { field: 'action', headerName: 'Action', minWidth: 80 , 
+        {
+            field: 'action', headerName: 'Action', minWidth: 80,
             sortable: false,
-            renderCell: renderAction(handleDelete),},
+            renderCell: renderAction(handleDelete),
+        },
     ]
     const sizeColumns = (handleDelete) => [
         { field: 'id', headerName: 'Sr.N.', width: 30 },
         { field: 'size', headerName: 'Size', flex: 1, minWidth: 100, },
-        { field: 'action', headerName: 'Action', minWidth: 80 , 
+        {
+            field: 'action', headerName: 'Action', minWidth: 80,
             sortable: false,
-            renderCell: renderAction(handleDelete),},
+            renderCell: renderAction(handleDelete),
+        },
     ]
 
     const { data, refetch } = useGetDataQuery({ endpoint: options_list })
@@ -117,10 +122,10 @@ const  OptionsList = ({ refreshTrigger }) => {
         return { id: index + 1, _id: row._id, color: toCamelCase(row?.name?.trim()), type: "color" };
     });
     const brandRow = data?.brands?.map((row, index) => {
-        return { id: index + 1,  _id: row._id, brand: toCamelCase(row?.name), type: "brand" };
+        return { id: index + 1, _id: row._id, brand: toCamelCase(row?.name), type: "brand" };
     });
     const sizeRow = data?.sizes?.map((row, index) => {
-        return { id: index + 1, _id: row._id, size: row?.name.toUpperCase() , type: "size"};
+        return { id: index + 1, _id: row._id, size: row?.name.toUpperCase(), type: "size" };
     });
 
 
@@ -133,7 +138,7 @@ const  OptionsList = ({ refreshTrigger }) => {
             await deleteData({ endpoint: `${delete_option}/${type}/${id}` }).unwrap().then(res => {
                 console.log(res);
                 refetch();
-                
+
             })
         } catch (error) {
             console.log(error, "error-------");
@@ -145,7 +150,7 @@ const  OptionsList = ({ refreshTrigger }) => {
             <Box className="page-title-box" mb={2}>
                 <Typography className='page-title' variant='h5'>Product Option Lists</Typography>
             </Box>
-            
+
             <Grid container spacing={2} mb={2}>
                 <Grid size={{ xs: 12, md: 4 }}>
                     <Card>
